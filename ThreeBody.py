7dabc13fca7f589aa9c15c95cc3c17792274acaa
AllanChain <txsmlf@gmail.com>
2018-06-04 16:21:38 +0800

    Try to use velocity verlet to make the simulation more accurate

--- ThreeBody.py
+++ ThreeBody.py
@@ -14,14 +14,22 @@ class Star:
         self.m=m
         self.v=v
         self.p=p
+        self._p=[p,p+v*DT]
+        a=[self.cacl_a]
         return
-    def setv(self,s1,s2):
-        a=(s1.m*(s1.p-self.p)/(s1.p-self.p).dis**3+\
+    @property
+    def p(self):
+        return self._p[1]
+    def calc_a(self,s1,s2):
+        self.a=(s1.m*(s1.p-self.p)/(s1.p-self.p).dis**3+\
                  s2.m*(s2.p-self.p)/(s2.p-self.p).dis**3)*G
+    def calc_p(self):
+        self._p.append(2*self.p-self._p.pop(0)+self.a*DT**2)
         #print(a.x,a.y,a.z)
-        self.v+=a*DT
+    def calc_v(self):
+        self.v+=(a*DT
         #print(self.p.x,self.p.y,self.p.z)
-        self.p+=self.v
+        self.p+=self.v*DT
         #print(self.p.x,self.p.y,self.p.z)
         #print('_'*20)
     def __str__(self):
@@ -32,6 +40,19 @@ class StarGroup:
     def __init__(self,stars,dis):
         self.stars=stars
         self.display=dis
+        self.M=0
+        for star in stars:
+            self.M+=star.m
+    def calc_rc(self):
+        r=v=V3(0,0,0)
+        E=0
+        for star in self.stars:
+            r+=star.p
+            v+=star.v
+            E+=1/2*star.m*star.v.dis**2
+        for i,j in ((0,1),(1,2),(0,2)):
+            E-=G*self.stars[i].m*self.stars[j].m/(self.stars[i].p-self.stars[j].p).dis
+        return r/self.M,v/self.M,E
     def proceed(self):
         self.stars[0].setv(self.stars[1],self.stars[2])
         self.stars[1].setv(self.stars[0],self.stars[2])
@@ -50,9 +71,18 @@ class StarGroup:
         info='='*20+'\n'
         for star in self.stars:
             info+=str(star)+'\n'+'-'*20+'\n'
+        rc,vc,E=self.calc_rc()
+        info+='rc:(%d,%d,%d),vc:(%d,%d,%d)'%(rc.x,rc.y,rc.z,vc.x,vc.y,vc.z)
+        info+='\nEnergy:'+str(E)
         return info
 def ranpro():
     return(V3(random()*2-1,random()*2-1,random()*2-1))
+def random_star():
+    s1=Star(1,ranpro()/10,ranpro()*5+V3(0,0,5))
+    s2=Star(1,ranpro()/10,ranpro()*5+V3(0,0,5))
+    s3=Star(1,V3(0,0,0)-s1.m*s1.v-s2.m*s2.v,V3(0,0,20)-s1.m*s1.p-s2.m*s2.p)
+    return StarGroup((s1,s2,s3),DISPLAY)
+
 def get_screen_pos(p):
     x,y=p.getv2(D)
     x+=SCREENX//2
@@ -60,11 +90,12 @@ def get_screen_pos(p):
     return (x,y)
 def main():
     global D,STARS
+    STARS=random_star()
     while True:
         time.sleep(0.1)
         STARS.proceed()
         STARS.draw()
-        if random()<0.2:
+        if random()<0.8:
             print(STARS)
         for event in pygame.event.get():
             if event.type==QUIT:
@@ -76,11 +107,6 @@ def main():
                     STARS.add(vec)
                     #print('done')
                 elif event.key==K_RETURN:
-                    return
-while True:
-    s1=Star(1,ranpro()/10,ranpro()*5+V3(0,0,5))
-    s2=Star(1,ranpro()/10,ranpro()*5+V3(0,0,5))
-    s3=Star(1,V3(0,0,0)-s1.m*s1.v-s2.m*s2.v,V3(0,0,20)-s1.m*s1.p-s2.m*s2.p)
-    STARS=StarGroup((s1,s2,s3),DISPLAY)
-    print('here')
-    main()
+                    print('*'*10+'NEW'+'*'*10)
+                    STARS=random_star()
+main()
