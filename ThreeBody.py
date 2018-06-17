71f6b3cc17348cb14dc644b885278dd6da4da9b8
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2018-06-17 17:01:14 +0800

    first trial for collision

--- ThreeBody.py
+++ ThreeBody.py
@@ -12,7 +12,7 @@ DISPLAY=None
 SCREENX,SCREENY=500,500
 D=30 #视距
 G=100 #引力常量
-DT=0.001 #每次循环三体的时间差Δt
+DT=0.01 #每次循环三体的时间差Δt
 WAIT=0.05 #每次pygame循环现实中经历时间
 WAIT_C=0.1 #每次命令行循环现实中经历时间
 #储存相互作用方式的字典
@@ -32,10 +32,12 @@ class Star:
     def calc_a(self,s1,s2):
         r1=(s1.p-self.p)
         r2=(s2.p-self.p)
+        if r1.dis<1:
+            self.group.crash(self,s1)
+        if r2.dis<1:
+            self.group.crash(self,s2)
         self.a=(s1.m*r1/r1.dis**3+\
                  s2.m*r2/r2.dis**3)*G
-        if r1.dis<0.5 or r2.dis<0.5:
-            print('#'*10+'CRASH')
         return self.a
     def calc_p(self):
         self._p.append(2*self.p-self._p.pop(0)+self.a*DT**2)
@@ -47,13 +49,13 @@ class Star:
 velocity:{self.v:.2f}
 screen_position:{get_screen_pos(self.p)}''')
 class StarGroup:
-    
     def __init__(self,stars,dis=None):
         self.stars=stars
         self.display=dis
         self.M=0
         for star in stars:
             self.M+=star.m
+            star.group=self
         for i,j in gravity_dict.items():
             self.stars[i].calc_a(self.stars[j[0]],self.stars[j[1]])
     def calc_rc(self):
@@ -71,7 +73,23 @@ class StarGroup:
             self.stars[i].calc_p()
         for i,j in gravity_dict.items():
             self.stars[i].calc_v(self.stars[j[0]],self.stars[j[1]])
-
+    def crash(self,s1,s2):
+        print('#'*10+'CRASH')
+        br=s1.p-s2.p
+        bv=s1.v-s2.v
+        if bv.dot(br)>0:
+            print(' '*5,bv)
+            v1,v2=s1.v,s2.v
+            m1,m2=s1.m,s2.m
+            v1n=(v1.dot(r))/r.dis
+            v1t=v1-v1t
+            v2n=(v2.dot(r))/r.dis
+            v2t=v2-v2t
+            v1nk=((m1-e*m2)*v1n+(1+e)*m2*v2n)/(m1+m2)
+            v2nk=((m2-e*m1)*v2n+(1+e)*m1*v1n)/(m1+m2)
+            s1.v=v1nk+v1t
+            s2.v=v2nk+v2t
+            print(' '*5,s1.v-s2.v)
     def draw(self):
         DISPLAY.fill((0,0,0))
         for star in self.stars:
