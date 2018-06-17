145abf3543363bb4a69d3d3aafbbfd8c81662f2e
AllanChain <txsmlf@gmail.com>
2018-06-17 17:38:21 +0800

    add acceleration output and PROTECT const.

--- ThreeBody.py
+++ ThreeBody.py
@@ -15,6 +15,7 @@ G=100 #引力常量
 DT=0.01 #每次循环三体的时间差Δt
 WAIT=0.05 #每次pygame循环现实中经历时间
 WAIT_C=0.1 #每次命令行循环现实中经历时间
+PROTECT=2#发生碰撞距离
 #储存相互作用方式的字典
 gravity_dict={0:(1,2),
                 1:(0,2),
@@ -32,9 +33,9 @@ class Star:
     def calc_a(self,s1,s2):
         r1=(s1.p-self.p)
         r2=(s2.p-self.p)
-        if r1.dis<1:
+        if r1.dis<PROTECT:
             self.group.crash(self,s1)
-        if r2.dis<1:
+        if r2.dis<PROTECT:
             self.group.crash(self,s2)
         self.a=(s1.m*r1/r1.dis**3+\
                  s2.m*r2/r2.dis**3)*G
@@ -47,6 +48,7 @@ class Star:
         return(
         f'''position:{self.p:.2f}
 velocity:{self.v:.2f}
+acceleration:{self.a:.2f}
 screen_position:{get_screen_pos(self.p)}''')
 class StarGroup:
     def __init__(self,stars,dis=None):
@@ -79,7 +81,6 @@ class StarGroup:
         e=1
         bv=s1.v-s2.v
         if bv.dot(r)>0:
-            print(' '*5,bv)
             v1,v2=s1.v,s2.v
             m1,m2=s1.m,s2.m
             v1n=(v1.dot(r)/r.square)*r
@@ -90,7 +91,10 @@ class StarGroup:
             v2nk=((m2-e*m1)*v2n+(1+e)*m1*v1n)/(m1+m2)
             s1.v=v1nk+v1t
             s2.v=v2nk+v2t
+            print(' '*5,bv)
             print(' '*5,s1.v-s2.v)
+            print(' '*5,v1n-v2n)
+            print(' '*5,v1nk-v2nk)
     def draw(self):
         DISPLAY.fill((0,0,0))
         for star in self.stars:
@@ -110,8 +114,8 @@ class StarGroup:
         info+=f'rc:{rc:.0f},vc:{vc:.0f}'
         info+='\nEnergy:'+str(E)
         if E>0:
-            for i in self.stars:
-                print(i.a)
+            #for i in self.stars:
+                #print(i.a)
             print(info)
             raise KeyboardInterrupt
         return info
